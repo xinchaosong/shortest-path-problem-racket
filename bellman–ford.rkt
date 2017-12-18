7017e1f530e6821b7cd9b1d9a356680d487aa7aa
Xinchao Song <31557601+xinchaosong@users.noreply.github.com>
2017-12-18 04:36:11 -0500

    Added some comments

--- bellman–ford.rkt
+++ bellman–ford.rkt
@@ -32,50 +32,54 @@ Purposes: Using Bellman–Ford algorithm to solve the shortest path problem
   ; returns the index of a node in node-list
   (define (node-index node)
     (index-of node-list node))
-  (cond [(not (and (member source node-list) (member target node-list))) #false]
-        [else
-         (let (; Step 1: initializes the graph
-               ; distance from source to source is 0 and from source to v is unknown
-               [dist (for/vector ([i node-list]) (if (symbol=? i source) 0 +inf.0))]
-               ; previous node in optimal path from source and from source to source is source itself
-               [prev (for/vector ([i node-list]) (if (symbol=? i source) source 'UNDEFINED))])
+  
+  (cond
+    ; if source or target is invaild, returns #false
+    [(not (and (member source node-list) (member target node-list))) #false]
+    ; otherwise solves the problem
+    [else
+     (let (; Step 1: initializes the graph
+           ; distance from source to source is 0 and from source to v is unknown
+           [dist (for/vector ([i node-list]) (if (symbol=? i source) 0 +inf.0))]
+           ; previous node in optimal path from source and from source to source is source itself
+           [prev (for/vector ([i node-list]) (if (symbol=? i source) source 'UNDEFINED))])
 
-           ; Step 2: relaxes edges repeatedly
-           (for ([n (in-range (- (length node-list) 1))])
-             (for ([i graph]) 
-               (let* ([u (car i)]
-                      [v (caddr i)]
-                      [weight (cadr i)]
-                      [alt (+ (vector-ref dist (node-index u)) weight)])
-                 ; a shorter path to v has been found
-                 (when (> (vector-ref dist (node-index v)) alt)
-                   ; updates the data of dist and prev
-                   (vector-set! dist (node-index v) alt)
-                   (vector-set! prev (node-index v) u)))))
+       ; Step 2: relaxes edges repeatedly
+       (for ([n (in-range (- (length node-list) 1))])
+         (for ([i graph]) 
+           (let* ([u (car i)]
+                  [v (caddr i)]
+                  [weight (cadr i)]
+                  [alt (+ (vector-ref dist (node-index u)) weight)])
+             ; a shorter path to v has been found
+             (when (> (vector-ref dist (node-index v)) alt)
+               ; updates the data of dist and prev
+               (vector-set! dist (node-index v) alt)
+               (vector-set! prev (node-index v) u)))))
 
-           ; Step 3: checks for negative-weight cycles
-           (for ([n (in-range (- (length node-list) 1))])
-             (for ([i graph]) 
-               (let* ([u (car i)]
-                      [v (caddr i)]
-                      [weight (cadr i)]
-                      [alt (+ (vector-ref dist (node-index u)) weight)])
-                 ; raises the exception if a negative-weight cycle has been found 
-                 (when (> (vector-ref dist (node-index v)) alt)
-                   (error "Graph contains a negative-weight cycle")))))
+       ; Step 3: checks for negative-weight cycles
+       (for ([n (in-range (- (length node-list) 1))])
+         (for ([i graph]) 
+           (let* ([u (car i)]
+                  [v (caddr i)]
+                  [weight (cadr i)]
+                  [alt (+ (vector-ref dist (node-index u)) weight)])
+             ; raises the exception if a negative-weight cycle has been found 
+             (when (> (vector-ref dist (node-index v)) alt)
+               (error "Graph contains a negative-weight cycle")))))
            
-           ; reads the shortest path from source to target
-           (let* ([u target]
-                  [prev-u (vector-ref prev (node-index u))])
-             ; returns false if there is no valid previous node found
-             (cond [(symbol=? prev-u 'UNDEFINED) #false] 
-                   [else
-                    ; constructs the shortest path with a stack
-                    (for/fold ([tail '()])
-                              ([i prev]
-                               ; pushes the source onto the stack at the end of the loop
-                               #:final (symbol=? prev-u u))
-                      ; pushes the vertex onto the stack and traverse from target to source
-                      (begin0 (cons u tail)
-                              (set! u prev-u)
-                              (set! prev-u (vector-ref prev (node-index u)))))])))]))
+       ; reads the shortest path from source to target
+       (let* ([u target]
+              [prev-u (vector-ref prev (node-index u))])
+         ; returns false if there is no valid previous node found
+         (cond [(symbol=? prev-u 'UNDEFINED) #false] 
+               [else
+                ; constructs the shortest path with a stack
+                (for/fold ([tail '()])
+                          ([i prev]
+                           ; pushes the source onto the stack at the end of the loop
+                           #:final (symbol=? prev-u u))
+                  ; pushes the vertex onto the stack and traverse from target to source
+                  (begin0 (cons u tail)
+                          (set! u prev-u)
+                          (set! prev-u (vector-ref prev (node-index u)))))])))]))
--- dijkstra.rkt
+++ dijkstra.rkt
@@ -32,56 +32,60 @@ Purposes: Using Dijkstra's algorithm to solve the shortest path problem
   ; returns the index of a node in node-list
   (define (node-index node)
     (index-of node-list node))
-  (cond [(not (and (member source node-list) (member target node-list))) #false]
-        [else
-         (let (; distance from source to source is 0 and from source to v is unknown
-               [dist (for/vector ([i node-list]) (if (symbol=? i source) 0 +inf.0))]
-               ; previous node in optimal path from source and from source to source is source itself
-               [prev (for/vector ([i node-list]) (if (symbol=? i source) source 'UNDEFINED))]
-               ; q-bool is a [Vector-of Boolean] that shows which nodes have not been processed
-               ; #true means that the corresponding node has not been processed and vice versa
-               [q-bool (make-vector (length node-list) #true)]
-               ; u is the node node currently processed
-               [u source])
+  
+  (cond
+    ; if source or target is invaild, returns #false
+    [(not (and (member source node-list) (member target node-list))) #false]
+    ; otherwise solves the problem
+    [else
+     (let (; distance from source to source is 0 and from source to v is unknown
+           [dist (for/vector ([i node-list]) (if (symbol=? i source) 0 +inf.0))]
+           ; previous node in optimal path from source and from source to source is source itself
+           [prev (for/vector ([i node-list]) (if (symbol=? i source) source 'UNDEFINED))]
+           ; q-bool is a [Vector-of Boolean] that shows which nodes have not been processed
+           ; #true means that the corresponding node has not been processed and vice versa
+           [q-bool (make-vector (length node-list) #true)]
+           ; u is the node node currently processed
+           [u source])
 
-           ; processes every node in node-list until the node currently processed is the end node
-           ; or all nodes have been processed
-           (for ([n node-list]
-                 #:break (symbol=? u target))
+       ; processes every node in node-list until the node currently processed is the end node
+       ; or all nodes have been processed
+       (for ([n node-list]
+             #:break (symbol=? u target))
            
-             ; node with the least distance will be processed first
-             (let* ([min-dist (argmin values (for/list ([i dist] [j q-bool] #:when j) i))]
-                    [min-index (vector-member min-dist dist)])
-               (set! u (list-ref node-list min-index)))
+         ; node with the least distance will be processed first
+         (let* ([min-dist (argmin values (for/list ([i dist] [j q-bool] #:when j) i))]
+                [min-index (vector-member min-dist dist)])
+           (set! u (list-ref node-list min-index)))
            
-             ; marks the node u as processed
-             (vector-set! q-bool (node-index u) #false)
+         ; marks the node u as processed
+         (vector-set! q-bool (node-index u) #false)
            
-             ; relaxes edges for each neighbor v of u
-             (for ([i graph])             
-               (let* ([v (caddr i)]
-                      [weight (cadr i)]
-                      [alt (+ (vector-ref dist (node-index u)) weight)])
-                 ; a shorter path to v has been found
-                 (when (and (symbol=? u (car i))
-                            (vector-ref q-bool (node-index v))
-                            (> (vector-ref dist (node-index v)) alt))
-                   ; updates the data of dist and prev
-                   (vector-set! dist (node-index v) alt)
-                   (vector-set! prev (node-index v) u)))))
+         ; relaxes edges for each neighbor v of u
+         (for ([i graph])             
+           (let* ([v (caddr i)]
+                  [weight (cadr i)]
+                  [alt (+ (vector-ref dist (node-index u)) weight)])
+             ; a shorter path to v has been found
+             (when (and (symbol=? u (car i))
+                        (vector-ref q-bool (node-index v))
+                        (> (vector-ref dist (node-index v)) alt))
+               ; updates the data of dist and prev
+               (vector-set! dist (node-index v) alt)
+               (vector-set! prev (node-index v) u)))))
          
-           ; reads the shortest path from source to target           
-           (let* ([u target]
-                  [prev-u (vector-ref prev (node-index u))])
-             ; returns false if there is no valid previous node found
-             (cond [(symbol=? prev-u 'UNDEFINED) #false] 
-                   [else
-                    ; constructs the shortest path with a stack
-                    (for/fold ([tail '()])
-                              ([i prev]
-                               ; pushes the source onto the stack at the end of the loop
-                               #:final (symbol=? prev-u u))
-                      ; pushes the vertex onto the stack and traverse from target to source
-                      (begin0 (cons u tail)
-                              (set! u prev-u)
-                              (set! prev-u (vector-ref prev (node-index u)))))])))]))
+       ; reads the shortest path from source to target           
+       (let* ([u target]
+              [prev-u (vector-ref prev (node-index u))])
+         ; returns false if there is no valid previous node found
+         (cond [(symbol=? prev-u 'UNDEFINED) #false] 
+               [else
+                ; constructs the shortest path with a stack
+                (for/fold ([tail '()])
+                          ([i prev]
+                           ; pushes the source onto the stack at the end of the loop
+                           #:final (symbol=? prev-u u))
+                  ; pushes the vertex onto the stack and traverse from target to source
+                  (begin0 (cons u tail)
+                          (set! u prev-u)
+                          (set! prev-u (vector-ref prev (node-index u)))))])))]))
